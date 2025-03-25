20280d644eb9c0ff711cd9eb7c94eb72fd23faca
tiennv-u2u <tiennv@uniultra.xyz>
2025-03-25 17:40:09 +0700

    ft: add bonus config type

---
+++ src/types/bonus_config.rs
@@ -0,0 +1,14 @@
+#[derive(Deserialize, Serialize, Clone, Debug, ToSchema)]
+pub struct BonusConfig {
+    country_geoname_id: i32,
+    bonus_amount: f64,
+}
+
+impl BonusConfig {
+    pub fn new(country_geoname_id: i32, bonus_amount: f64) -> Self {
+        Self {
+            country_geoname_id,
+            bonus_amount,
+        }
+    }
+}
