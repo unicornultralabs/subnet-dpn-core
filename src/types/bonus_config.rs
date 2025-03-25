8c52070b3ba28a9fcfb1e751fd2c33e63eaf8485
tiennv-u2u <tiennv@uniultra.xyz>
2025-03-25 17:46:34 +0700

    chore: update type

--- src/types/bonus_config.rs
+++ src/types/bonus_config.rs
@@ -3,15 +3,24 @@ use utoipa::ToSchema;
 
 #[derive(Deserialize, Serialize, Clone, Debug, ToSchema)]
 pub struct BonusConfig {
-    country_geoname_id: i32,
-    bonus_amount: f64,
+    pub country_geoname_id: i32,
+    pub bonus_amount: f64,
+    pub created_at: i64,
+    pub updated_at: i64,
 }
 
 impl BonusConfig {
-    pub fn new(country_geoname_id: i32, bonus_amount: f64) -> Self {
+    pub fn new(
+        country_geoname_id: i32,
+        bonus_amount: f64,
+        created_at: i64,
+        updated_at: i64,
+        ) -> Self {
         Self {
             country_geoname_id,
             bonus_amount,
+            created_at,
+            updated_at,
         }
     }
 }
