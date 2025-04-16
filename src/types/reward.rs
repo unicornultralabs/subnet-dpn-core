4c03d28d1f25bd73a5ddae103751977fcadf6b85
leedev-u2u <lamnx@uniultra.xyz>
2025-04-16 17:13:39 +0700

    update reward v2 (#82)
    Co-authored-by: Tlee <Tlee>

--- src/types/reward.rs
+++ src/types/reward.rs
@@ -30,10 +30,12 @@ pub struct RewardsOverviewV2 {
     pub unclaimed_rewards_v2: i64,
     /// claimed + unclaimed
     pub total_network_rewards: i64,
+    pub total_network_rewards_v2: i64,
     /// claimed + unclaimed
     pub total_task_rewards: i64,
     /// claimed + unclaimed
     pub total_referral_rewards: i64,
+    pub total_referral_rewards_v2: i64,
     /// claimed + unclaimed
     pub total_commission_rewards: i64,
 }
